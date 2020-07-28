795b86e7747c5bce63072f65543d83f849c82335
Jan Vogel <github@janvogel.org>
2020-07-28 17:12:15 +0200

    Add build support for the ESP8266
    Signed-off-by: Jan Vogel <github@janvogel.org>

--- ci/build-arduino.sh
+++ ci/build-arduino.sh
@@ -19,8 +19,9 @@ curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.
 arduino-cli config init
 arduino-cli core update-index
 
-# Install Arduino AVR core
+# Install Arduino AVR core and ESP8266 core
 arduino-cli core install arduino:avr
+arduino-cli core install esp8266:esp8266
 
 # Install the required libraries
 arduino-cli lib install RTClib
@@ -30,6 +31,11 @@ arduino-cli lib install U8glib
 patch -p0 -d $HOME/Arduino/libraries <$GITHUB_WORKSPACE/U8glib.patch
 
 # Compile all *.ino files for the Arduino Leonardo
-for f in **/*.ino ; do
+for f in wifi_watering_kit_code/*.ino ; do
   arduino-cli compile -b arduino:avr:leonardo $f
+done
+
+# Compile all *.ino files for the ESP8266
+for f in ESP8266_watering_mqtt_client_code/*.ino ; do
+  arduino-cli compile -b arduino:esp8266:esp8266 $f
 done
